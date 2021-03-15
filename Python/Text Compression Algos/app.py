349b860fe7c1ebfa71dc4cea2bcee994daa43bf1
Thamarai-Selvam <sthamarai001@gmail.com>
2021-03-15 20:26:57 +0530

    Compression Algos- File Compression Added

--- Python/Text Compression Algos/app.py
+++ Python/Text Compression Algos/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, Response, render_template, send_file,Markup
 from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
 from hurry.filesize import size
 from Huffman_Coding.app import compress_file, decompress_file
-
+from Run_Length_Coding.app import encode, decode
+from LZW_Coding.app import compress, deCompress
 app = Flask(__name__)
 sFileSize = fFileSize = 0
+
 def huffman(idata):
     global sFileSize, fFileSize
     if idata :
@@ -27,19 +30,31 @@ def dchuffman(idata):
 
 def runLength(idata):
 
-    return "Success"
+    return encode(idata)
 
 def lZW(idata):
 
-    return "Success"
+    global sFileSize, fFileSize
+    if idata :
+        filename = secure_filename(idata.filename)
+        print(filename)
+    sFileSize, fFileSize = compress(idata, str(idata.filename+'.cmp'))
+    # return str(sFileSize), str(fFileSize)
+    return send_file(str(idata.filename+'.cmp'), as_attachment=True)
     
 def dcrunLength(idata):
 
-    return "Success"
+    return decode(idata)
 
 def dclZW(idata):
 
-    return "Success"
+    global sFileSize, fFileSize
+    if idata :
+        filename = secure_filename(idata.filename)
+        print(filename[0:-4])
+    fFileSize,sfFileSize = deCompress(idata, str(idata.filename[0:-4]))
+    # return str(sFileSize), str(fFileSize)
+    return send_file(str(idata.filename[0:-4]), as_attachment=True)
 
 @app.route('/downFile', methods=['GET', 'POST'])
 def downloadFile():
@@ -48,6 +63,7 @@ def downloadFile():
     print(filename)
     return send_file(filename, as_attachment=True)
     
+
 @app.route('/dcdownFile', methods=['GET', 'POST'])
 def dcdownloadFile():
    
@@ -55,45 +71,82 @@ def dcdownloadFile():
     print(filename)
     return send_file(filename, as_attachment=True)
 
+
 @app.route('/compress', methods=['GET', 'POST'])
 def getHandler():
     global sFileSize, fFileSize
     sFileSize = fFileSize = 0
-    print(request.args)
+    print(request.form)
     opChoice = int(request.form['opChoice'])
     opCmpChoice = int(request.form['opCmpChoice'])
     idata = request.form['itext']
-    if 'ifile' in request.files:
+    tFile = request.files['ifile']
+    print(request.files)
+    if tFile :
+        print('file is there-> ', tFile)
         idata = request.files['ifile']
     print(idata)
     if (opCmpChoice == 0):
-        switcher = { 
-            0: huffman(idata), 
-            1: runLength(idata), 
-            2: lZW(idata), 
-        }     
-
-        iresult = switcher.get(opChoice,('Invalid data supplied', 500))
-        form = Markup('<form class="response_form" id="response_form" action="/downFile" method=post enctype=multipart/form-data>\
+        if(opChoice == 0):
+            huffman(idata)
+            form = Markup('<form class="response_form" id="response_form" action="/downFile" method=post enctype=multipart/form-data>\
                 <label for="ofile">UnCompressed File Size '+str(size(sFileSize))+'</label></br>\
                 <label for="cfile">Compressed File Size '+str(size(fFileSize))+'</label></br>\
                 <input type="hidden" id="itextVal" name="itextVal" value=""></br>\
                 <input type="submit" id="dbtn" name="dbtn" value="Download File"></br>\
             </form>')
+        elif(opChoice == 1):
+            cText = runLength(idata)
+            form = Markup('<form class="response_form" id="response_form" action="/dcdownFile" method=post enctype=multipart/form-data>\
+                <label for="ofile">UnCompressed Text '+str(idata)+'</label></br>\
+                <label for="cfile">Compressed Text '+str(cText)+'</label></br>\
+                <input type="hidden" id="itextVal" name="itextVal" value=""></br>\
+            </form>')
+        else:
+            lZW(idata)
+            form = Markup('<form class="response_form" id="response_form" action="/downFile" method=post enctype=multipart/form-data>\
+                <label for="ofile">UnCompressed File Size '+str(size(sFileSize))+'</label></br>\
+                <label for="cfile">Compressed File Size '+str(size(fFileSize))+'</label></br>\
+                <input type="hidden" id="itextVal" name="itextVal" value=""></br>\
+                <input type="submit" id="dbtn" name="dbtn" value="Download File"></br>\
+            </form>')
+        
+
+        # iresult = switcher.get(opChoice,('Invalid data supplied', 500))
+        # print(iresult)
+       
         return render_template('index.html',resp_form=form)
     else:
-        switcher = { 
-            0: dchuffman(idata), 
-            1: dcrunLength(idata), 
-            2: dclZW(idata), 
-        }     
-        iresult = switcher.get(opChoice,('Invalid data supplied', 500))
-        form = Markup('<form class="response_form" id="response_form" action="/dcdownFile" method=post enctype=multipart/form-data>\
+        if(opChoice == 0):
+            dchuffman(idata)
+            form = Markup('<form class="response_form" id="response_form" action="/dcdownFile" method=post enctype=multipart/form-data>\
                 <label for="ofile">UnCompressed File Size '+str(size(sFileSize))+'</label></br>\
                 <label for="cfile">Compressed File Size '+str(size(fFileSize))+'</label></br>\
                 <input type="hidden" id="itextVal" name="itextVal" value=""></br>\
                 <input type="submit" id="dbtn" name="dbtn" value="Download File"></br>\
             </form>')
+        elif(opChoice == 1):
+            dcText = dcrunLength(idata)
+            form = Markup('<form class="response_form" id="response_form" action="/dcdownFile" method=post enctype=multipart/form-data>\
+                <label for="ofile">UnCompressed Text '+str(idata)+'</label></br>\
+                <label for="cfile">Compressed Text '+str(dcText)+'</label></br>\
+                <input type="hidden" id="itextVal" name="itextVal" value=""></br>\
+            </form>')
+        else:
+            dclZW(idata)
+            form = Markup('<form class="response_form" id="response_form" action="/dcdownFile" method=post enctype=multipart/form-data>\
+                <label for="ofile">UnCompressed File Size '+str(size(sFileSize))+'</label></br>\
+                <label for="cfile">Compressed File Size '+str(size(fFileSize))+'</label></br>\
+                <input type="hidden" id="itextVal" name="itextVal" value=""></br>\
+                <input type="submit" id="dbtn" name="dbtn" value="Download File"></br>\
+            </form>')
+        # switcher = { 
+        #     0: dchuffman(idata), 
+        #     1: dcrunLength(idata),  
+        #     2: dclZW(idata), 
+        # }     
+        # iresult = switcher.get(opChoice,('Invalid data supplied', 500))
+        
         return render_template('index.html',resp_form=form)
 
 
