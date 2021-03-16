2a31efad78e1cc04096c0e056e7622a2d6fb3375
Thamarai-Selvam <sthamarai001@gmail.com>
2021-03-16 21:14:04 +0530

    refactoredCode@eMailConfirmation

--- Python/libraryPortal/app.py
+++ Python/libraryPortal/app.py
@@ -11,10 +11,11 @@ mail = Mail(app)
 s = URLSafeTimedSerializer('Thisisasecret!')
 
 tempData = {}
+allUserData = {}
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def index():
-    global tempData
+    global tempData, allUserData
     if request.method == 'GET':
         # return '<form action="/" method="POST"><input name="email"><input type="submit"></form>'
         return render_template("index.html") 
@@ -23,22 +24,20 @@ def index():
         userName = request.form['userName']
         phNum = request.form['phNum']
         rollNum = request.form['rollNum']
-
+        passKey = request.form['passKey']
         tempData = { 
                     'userName' : userName,
                     'phNum' : phNum,
                     'rollNum' : rollNum,
-                    'eMail' : email
+                    'eMail' : email,
+                    'passKey' : passKey
                     }
         print(tempData)
         token = s.dumps(email, salt='email-confirm')
 
         msg = Message('Confirm Email', sender='meliodastheman106@gmail.com', recipients=[email])
-
         link = url_for('confirm_email', token=token, _external=True)
-
         msg.body = 'Your link is {}'.format(link)
-
         mail.send(msg)
 
         return '<h1>The email you entered is {}. The token is {}</h1>'.format(email, token)
@@ -56,16 +55,58 @@ def confirm_email(token):
     userName = userdata["userName"]
     phNum = userdata["phNum"]
     eMail = userdata["eMail"]
+    passKey = userdata["passKey"]
     regOn = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     uID = str(uuid.uuid4())
 
     if len(rollNum) < 2 and len(userName) < 3 and len(phNum) < 9 and len(eMail) < 10 :
         return "Please submit valid data."
     else:
+        for userRow in allUserData:
+            print(f"postRoll {rollNum} | userRoll {userRow['rollNum']}")
+            if(int(rollNum) == int(userRow['rollNum'])):
+                return '<h1>User Already Registered !</h1>'
+
         with open('data.csv', mode='a') as csv_file:
             data = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            data.writerow([uID, rollNum, userName, phNum, eMail, regOn])
+            data.writerow([uID, rollNum, userName, phNum, eMail, regOn, passKey])
         return '<h1>Registered Successfully !</h1>'
 
+@app.route('/', methods=['GET', 'POST'])
+def cLogin():
+    if request.method == 'POST':
+        bRollNum = request.form['rollNum']
+        bPassKey = request.form['passKey']
+        print(allUserData)
+        for userRow in allUserData:
+            print(userRow)
+            print(f"postRoll {bRollNum} | userRoll {userRow['rollNum']}")
+            if(int(bRollNum) == int(userRow['rollNum'])):
+                if(bPassKey == userRow['passKey']):
+                    return render_template("dash.html", userData = userRow) 
+    else:
+        return render_template('login.html')
+
+def fileLoader():
+    global allUserData
+    with open('data.csv') as csv_file:
+            data = csv.reader(csv_file, delimiter=',')
+            first_line = True
+            allUserData = []
+            for row in data:
+                
+                if not first_line:
+                    print(row)
+                    allUserData.append({ 
+                        'rollNum' : row[1],
+                        'userName' : row[2],
+                        'phNum' : row[3],
+                        'eMail' : row[4],
+                        'passKey' : row[6]
+                    })
+                else:
+                    first_line = False
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    fileLoader()
+    app.run(debug=True)     
