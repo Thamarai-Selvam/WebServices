6390e4da05dbcc6133b7719f96b8df15bae117dd
Thamarai-Selvam <sthamarai001@gmail.com>
2021-03-16 18:44:00 +0530

    userRegistration - mailConfirmation

--- Python/libraryPortal/app.py
+++ Python/libraryPortal/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, url_for, render_template
 from flask_mail import Mail, Message
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+import uuid, csv, datetime
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
@@ -9,24 +10,38 @@ mail = Mail(app)
 
 s = URLSafeTimedSerializer('Thisisasecret!')
 
+tempData = {}
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global tempData
     if request.method == 'GET':
         # return '<form action="/" method="POST"><input name="email"><input type="submit"></form>'
         return render_template("index.html") 
+    else:
+        email = request.form['email']
+        userName = request.form['userName']
+        phNum = request.form['phNum']
+        rollNum = request.form['rollNum']
 
-    email = request.form['email']
-    token = s.dumps(email, salt='email-confirm')
+        tempData = { 
+                    'userName' : userName,
+                    'phNum' : phNum,
+                    'rollNum' : rollNum,
+                    'eMail' : email
+                    }
+        print(tempData)
+        token = s.dumps(email, salt='email-confirm')
 
-    msg = Message('Confirm Email', sender='meliodastheman106@gmail.com', recipients=[email])
+        msg = Message('Confirm Email', sender='meliodastheman106@gmail.com', recipients=[email])
 
-    link = url_for('confirm_email', token=token, _external=True)
+        link = url_for('confirm_email', token=token, _external=True)
 
-    msg.body = 'Your link is {}'.format(link)
+        msg.body = 'Your link is {}'.format(link)
 
-    mail.send(msg)
+        mail.send(msg)
 
-    return '<h1>The email you entered is {}. The token is {}</h1>'.format(email, token)
+        return '<h1>The email you entered is {}. The token is {}</h1>'.format(email, token)
 
 @app.route('/confirm_email/<token>')
 def confirm_email(token):
@@ -34,7 +49,23 @@ def confirm_email(token):
         email = s.loads(token, salt='email-confirm', max_age=3600)
     except SignatureExpired:
         return '<h1>The token is expired!</h1>'
-    return '<h1>The token works!</h1>'
+    
+    userdata = dict(tempData)
+    print(userdata)
+    rollNum = userdata["rollNum"]
+    userName = userdata["userName"]
+    phNum = userdata["phNum"]
+    eMail = userdata["eMail"]
+    regOn = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    uID = str(uuid.uuid4())
+
+    if len(rollNum) < 2 and len(userName) < 3 and len(phNum) < 9 and len(eMail) < 10 :
+        return "Please submit valid data."
+    else:
+        with open('data.csv', mode='a') as csv_file:
+            data = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            data.writerow([uID, rollNum, userName, phNum, eMail, regOn])
+        return '<h1>Registered Successfully !</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
