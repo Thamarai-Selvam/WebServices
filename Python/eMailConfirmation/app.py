1b3bdcb5c213df105c9a7e4480d0f9584dc6929e
Thamarai-Selvam <sthamarai001@gmail.com>
2021-03-16 13:19:48 +0530

    portalRegAndPasswordChangeConfirmation

--- Python/eMailConfirmation/app.py
+++ Python/eMailConfirmation/app.py
@@ -1,19 +1,15 @@
 import email, smtplib, ssl
-from email import encoders
-from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+password = "meliodas1063000"
 
-def sendMailtoRecipient(senderMail, receiverMail):
+def sendMailtoRecipient(senderMail = "meliodastheman106@gmail.com", receiverMail = "sthamarai001@gmail.com"):
     subject = "Test eMail"
     body = "This is test email"
-    sender_email = "meliodastheman106@gmail.com"
-    receiver_email = "sthamarai001@gmail.com"
-    password = "meliodas1063000"
     # Create the email head (sender, receiver, and subject)
     email = MIMEMultipart()
-    email["From"] = sender_email
-    email["To"] = receiver_email 
+    email["From"] = senderMail
+    email["To"] = receiverMail 
     email["Subject"] = subject
     # Add body and attachment to email
     email.attach(MIMEText(body, "plain"))
@@ -21,9 +17,9 @@ def sendMailtoRecipient(senderMail, receiverMail):
     #Create SMTP session for sending the mail
     session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
     session.starttls() #enable security
-    session.login(sender_email, password) #login with mail_id and password
+    session.login(senderMail, password) #login with mail_id and password
     text = email.as_string()
-    session.sendmail(sender_email, receiver_email, text)
+    session.sendmail(senderMail, receiverMail, text)
     session.quit()
     print('Mail Sent')
 
